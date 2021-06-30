e2b94b9e7c9d48f5b3c8c48ab5b070d2f95b2e8c
agustinvarela <84751360+agustinvarela@users.noreply.github.com>
2021-06-30 20:46:06 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require ('express');
 const app = express();
 const path = require('path');
-app.listen (3000, () => console.log ('Server Start', 'http://localhost:3000'));
+
+app.set("port",process.env.PORT || 3000)
+
+app.listen (app.get("port"),() => console.log ("Server Start http://localhost:"+app.get("port")))
 
 app.use(express.static (path.resolve(__dirname, '../public')))
 
