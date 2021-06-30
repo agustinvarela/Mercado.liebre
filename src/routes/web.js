906682d7c823c55b22a7bd33c4807988c6df144c
agustinvarela <84751360+agustinvarela@users.noreply.github.com>
2021-06-30 19:32:16 -0300

    Add files via upload
    dasdas

---
+++ src/routes/web.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const app = express.Router();
+const path = require('path');
+
+app.get('/', (req,res) => res.sendFile (path.resolve(__dirname,'../views','index.html')))
+
+app.get('/login', (req,res) => res.sendFile (path.resolve(__dirname,'../views/login.html')))
+
+app.get('/register', (req,res) => res.sendFile (path.resolve(__dirname,'../views/register.html')))
+
+module.exports = app; 
